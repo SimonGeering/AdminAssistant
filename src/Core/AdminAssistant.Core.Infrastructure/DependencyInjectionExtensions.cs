d5b4f4d14d0ad4afeff017f48eb2849baac962b4
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-03-14 18:07:57 +0000

    #1: Added Admin Assistant Core Infrastructure dll project implementing cross-cutting plumbing service interfaces

---
+++ src/Core/AdminAssistant.Core.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AdminAssistant.Core.Infrastructure.DomainModel;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddCoreInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IConfigurationManager, ConfigurationManager>();
+            services.AddTransient<IUserContextProvider, UserContextProvider>();
+        }
+    }
+}
+
---
+++ src/Core/AdminAssistant.Core.Infrastructure/DomainModel/UserContextProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminAssistant.Core.Infrastructure.DomainModel
+{
+    public class UserContextProvider : IUserContextProvider
+    {
+        public User GetCurrentUser()
+        {
+            // TODO: Implement IUserContextProvider.GetCurrentUser;
+            return new User() { SignOn = "simongeering" };
+        }
+    }
+}
