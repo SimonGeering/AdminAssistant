0306606d383302c0c5386dfccce8f77b1dd484fd
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-04-27 23:28:00 +0100

    #31 WIP switching Bank Account components to Syncfusion

--- src/AdminAssistant/UI/Modules/Accounts/BankAccountRightSidebar/IBankAccountRightSidebarViewModel.cs
+++ src/AdminAssistant/UI/Modules/Accounts/BankAccountRightSidebar/IBankAccountRightSidebarViewModel.cs
@@ -2,5 +2,6 @@ namespace AdminAssistant.UI.Modules.Accounts.BankAccountRightSidebar
 {
     public interface IBankAccountRightSidebarViewModel : IViewModelBase
     {
+        void OnAddAccountButtonClick();
     }
 }
