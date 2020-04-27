8e90032c2f005c4e4913725cfd6456ec8fcde2cf
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-04-27 23:40:35 +0100

    #31 Updated markup to use new component naming

--- src/AdminAssistant.UI/Modules/Accounts/BankAccountRightSidebar/BankAccountRightSidebarViewModel.cs
+++ src/AdminAssistant.UI/Modules/Accounts/BankAccountRightSidebar/BankAccountRightSidebarViewModel.cs
@@ -2,5 +2,9 @@ namespace AdminAssistant.UI.Modules.Accounts.BankAccountRightSidebar
 {
     public class BankAccountRightSidebarViewModel : ViewModelBase, IBankAccountRightSidebarViewModel
     {
+        public void OnAddAccountButtonClick()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
