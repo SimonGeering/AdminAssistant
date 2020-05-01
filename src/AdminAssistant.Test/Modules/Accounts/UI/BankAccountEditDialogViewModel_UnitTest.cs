2d00311551a48686f3e45fa1fd9d558f8cec236a
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-01 14:25:48 +0100

    #31 Added unit test for InitializeAsync method of the BankAccountEditDialogViewModel

--- src/AdminAssistant.Test/Modules/Accounts/UI/BankAccountEditDialogViewModel_UnitTest.cs
+++ src/AdminAssistant.Test/Modules/Accounts/UI/BankAccountEditDialogViewModel_UnitTest.cs
@@ -8,6 +8,9 @@ using AdminAssistant.DomainModel.Modules.Accounts;
 using AdminAssistant.UI.Modules.Accounts.BankAccountEditDialog;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using AdminAssistant.UI.Modules.Accounts;
+using AdminAssistant.Framework.Providers;
+using Moq;
+using System.Threading;
 
 namespace AdminAssistant.Accounts.Modules.Accounts.UI
 {
@@ -15,12 +18,48 @@ namespace AdminAssistant.Accounts.Modules.Accounts.UI
     {
         [Fact]
         [Trait("Category", "Unit")]
+        public async Task Have_All_Lookups_Populated_Given_TheScreenHasLoaded()
+        {
+            // Arrange
+            var mockHttpClientJsonProvider = new Mock<IHttpClientJsonProvider>();
+
+            mockHttpClientJsonProvider
+                .Setup(x => x.GetFromJsonAsync<BankAccountType[]>("api/v1/BankAccountType", It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new BankAccountType[]
+                {
+                    TestData.BankAccountTypeBuilder.WithTestData().Build()
+                }));
+
+            mockHttpClientJsonProvider
+                .Setup(x => x.GetFromJsonAsync<Currency[]>("api/v1/Currency", It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new Currency[]
+                {
+                    TestData.CurrencyBuilder.WithTestData().Build()
+                }));
+
+            var services = new ServiceCollection();
+            services.AddMocksOfExternalDependencies();
+            services.AddAdminAssistantClientServices();
+            services.AddTransient((sp) => mockHttpClientJsonProvider.Object);
+
+            // Act
+            var vm = services.BuildServiceProvider().GetRequiredService<IBankAccountEditDialogViewModel>();
+            await vm.InitializeAsync().ConfigureAwait(false);
+
+            // Assert
+            vm.BankAccountTypes.Should().NotBeNullOrEmpty();
+            vm.Currencies.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
         public void Have_AnEditHeader_GivenAnExistingValidBankAccount()
         {
             // Arrange
             var services = new ServiceCollection();
-            services.AddAdminAssistantUI();
-            services.AddAdminAssistantClientSideDomainModel();
+            services.AddMocksOfExternalDependencies();
+            services.AddAdminAssistantClientServices();
+
             var container = services.BuildServiceProvider();
 
             // Act
@@ -37,8 +76,9 @@ namespace AdminAssistant.Accounts.Modules.Accounts.UI
         {
             // Arrange
             var services = new ServiceCollection();
-            services.AddAdminAssistantUI();
-            services.AddAdminAssistantClientSideDomainModel();
+            services.AddMocksOfExternalDependencies();
+            services.AddAdminAssistantClientServices();
+
             var container = services.BuildServiceProvider();
 
             // Act
