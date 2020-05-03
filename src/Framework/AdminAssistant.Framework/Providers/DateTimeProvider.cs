c7c8154c66c7a42d5747e70f0311aaaf9490e11d
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-03 17:53:45 +0100

    #18 Added missing xml comment docs

--- src/Framework/AdminAssistant.Framework/Providers/DateTimeProvider.cs
+++ src/Framework/AdminAssistant.Framework/Providers/DateTimeProvider.cs
@@ -4,10 +4,15 @@ namespace AdminAssistant.Framework.Providers
 {
     public interface IDateTimeProvider
     {
+        ///<summary>
+        ///Gets a System.DateTime object that is set to the current date and time on this
+        ///computer, expressed as the Coordinated Universal Time (UTC).
+        ///</summary>
+        ///<returns>An object whose value is the current UTC date and time.</returns>
         DateTime UtcNow { get; }
     }
     public class DateTimeProvider : IDateTimeProvider
     {
-        public DateTime UtcNow => System.DateTime.UtcNow;
+        public DateTime UtcNow => DateTime.UtcNow;
     }
 }
