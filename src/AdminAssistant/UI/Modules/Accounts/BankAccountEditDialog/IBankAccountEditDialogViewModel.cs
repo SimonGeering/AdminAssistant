33821419f739c50e385686eff955842c4e89b0eb
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-09 22:48:16 +0100

    #31: Removed EditForm based validation, and two-way data binding in favor of one way binding and value change handlers to allow UI logic to be abstracted into viewmodel independent of UI tech used.

--- src/AdminAssistant/UI/Modules/Accounts/BankAccountEditDialog/IBankAccountEditDialogViewModel.cs
+++ src/AdminAssistant/UI/Modules/Accounts/BankAccountEditDialog/IBankAccountEditDialogViewModel.cs
@@ -1,16 +1,11 @@
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using AdminAssistant.DomainModel.Modules.Accounts;
-using AdminAssistant.UI.Shared;
 
 namespace AdminAssistant.UI.Modules.Accounts.BankAccountEditDialog
 {
     public interface IBankAccountEditDialogViewModel : IViewModelBase
     {
-        ILoadingSpinner LoadingSpinner { get; }
-
-        BankAccount BankAccount { get; }
+        BankAccount Model { get; }
         string HeaderText { get; }
         bool ShowDialog { get; set; }
 
@@ -20,9 +15,11 @@ namespace AdminAssistant.UI.Modules.Accounts.BankAccountEditDialog
         void OnSaveButtonClick();
         void OnCancelButtonClick();
 
-        Task InitializeAsync();
+        void OnAccountNameChanged(string accountName);
+        string AccountNameValidationMessage { get; }
+        string AccountNameValidationClass { get; }
 
-        void OnValidate();
-        event Action? Validate;
+        void OnBankAccountTypeChanged();
+        void OnCurrencyChanged();
     }
 }
