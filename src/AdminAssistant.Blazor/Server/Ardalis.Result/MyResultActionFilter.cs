ea390d5d08a74e2e63feebf1f2e417365c611cdd
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-13 15:22:47 +0100

    #48 Added Ardalis.Result MyResultActionFilter in lue of NuGet package.

---
+++ src/AdminAssistant.Blazor/Server/Ardalis.Result/MyResultActionFilter.cs
@@ -0,0 +1,39 @@
+//----------------------------------------------------------------------------------------------------------------------
+//     Author:  Steve Smith (https://github.com/ardalis)
+//     GitHub:  https://github.com/ardalis/Result/blob/master/sample/Ardalis.Result.SampleWeb/MyResultActionFilter.cs
+//     License: MIT
+//----------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Ardalis.Result.SampleWeb
+{
+    // TODO: Move to an Ardalis.Result.AspNetCore Nuget package
+    public class MyResultActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!((context.Result as ObjectResult).Value is IResult result)) return;
+
+            if (!(context.Controller is ControllerBase controller)) return;
+
+            if (result.Status == ResultStatus.NotFound)
+                context.Result = controller.NotFound();
+
+            if (result.Status == ResultStatus.Invalid)
+            {
+                foreach (var error in result.ValidationErrors)
+                {
+                    (context.Controller as ControllerBase).ModelState.AddModelError(error.Key, error.Value);
+                }
+
+                context.Result = controller.BadRequest(controller.ModelState);
+            }
+
+            if (result.Status == ResultStatus.Ok)
+            {
+                context.Result = new OkObjectResult(result.GetValue());
+            }
+        }
+    }
+}
