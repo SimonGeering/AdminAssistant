79eeff0b7043f9ab4c727ab759e08d7de6061a37
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-13 15:21:37 +0100

    #48 Added Ardalis.Result classes to framework in lue of NuGet package.

---
+++ src/Framework/AdminAssistant.Framework/Ardalis.Result/ResultStatus.cs
@@ -0,0 +1,16 @@
+//----------------------------------------------------------------------------------------------------------------------
+//     Author:  Steve Smith (https://github.com/ardalis)
+//     GitHub:  https://github.com/ardalis/Result/blob/master/src/Ardalis.Result/ResultStatus.cs
+//     License: MIT
+//----------------------------------------------------------------------------------------------------------------------
+namespace Ardalis.Result
+{
+    public enum ResultStatus
+    {
+        Ok,
+        Error,
+        Forbidden,
+        Invalid,
+        NotFound
+    }
+}
---
+++ src/Framework/AdminAssistant.Framework/Ardalis.Result/Results.cs
@@ -0,0 +1,64 @@
+//----------------------------------------------------------------------------------------------------------------------
+//     Author:  Steve Smith (https://github.com/ardalis)
+//     GitHub:  https://github.com/ardalis/Result/blob/master/src/Ardalis.Result/Result.cs
+//     License: MIT
+//----------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+namespace Ardalis.Result
+{
+    public partial class Result<T> : IResult
+    {
+        public Result(T value)
+        {
+            Value = value;
+        }
+        private Result(ResultStatus status)
+        {
+            Status = status;
+        }
+
+        public T Value { get; }
+        public Type ValueType
+        {
+            get
+            {
+                return Value.GetType();
+            }
+        }
+        public ResultStatus Status { get; } = ResultStatus.Ok;
+        public IEnumerable<string> Errors { get; private set; } = new List<string>();
+        public Dictionary<string, string> ValidationErrors { get; private set; } = new Dictionary<string, string>();
+
+        public object GetValue()
+        {
+            return this.Value;
+        }
+
+        public static Result<T> Success(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public static Result<T> Error(params string[] errorMessages)
+        {
+            return new Result<T>(ResultStatus.Error) { Errors = errorMessages };
+        }
+
+        public static Result<T> Invalid(Dictionary<string, string> validationErrors)
+        {
+            return new Result<T>(ResultStatus.Invalid) { ValidationErrors = validationErrors };
+        }
+
+        public static Result<T> NotFound()
+        {
+            return new Result<T>(ResultStatus.NotFound);
+        }
+
+        public static Result<T> Forbidden()
+        {
+            return new Result<T>(ResultStatus.Forbidden);
+        }
+    }
+}
