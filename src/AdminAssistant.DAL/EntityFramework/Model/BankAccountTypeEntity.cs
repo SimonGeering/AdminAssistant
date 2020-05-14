f4fbafb4a611cf929e2806519f0e586a8c7f08da
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-05-14 01:29:05 +0100

    #31 Split IMapping into IMapFrom and IMapTo to ovoid method name ambiguity error with non-custom two way mappings.

--- src/AdminAssistant.DAL/EntityFramework/Model/BankAccountTypeEntity.cs
+++ src/AdminAssistant.DAL/EntityFramework/Model/BankAccountTypeEntity.cs
@@ -3,7 +3,7 @@ using AdminAssistant.Framework.TypeMapping;
 
 namespace AdminAssistant.DAL.EntityFramework.Model
 {
-    public class BankAccountTypeEntity : IMapping<BankAccountTypeEntity, BankAccountType>
+    public class BankAccountTypeEntity : IMapTo<BankAccountType>
     {
         public int BankAccountTypeID { get; set; }
         public string Description { get; set; } = string.Empty;
--- src/Framework/AdminAssistant.Framework/TypeMapping/IMapping.cs
+++
@@ -1,9 +0,0 @@
-using AutoMapper;
-
-namespace AdminAssistant.Framework.TypeMapping
-{
-    public interface IMapping<TSource, TDestination>
-    {
-        void Map(Profile profile) => profile.CreateMap(typeof(TSource), typeof(TDestination));
-    }
-}
