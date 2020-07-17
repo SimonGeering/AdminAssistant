3ddb3df4a166447f6846e46787ce36424d72c348
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-07-18 00:28:18 +0100

    Added Syncfusion windows styling

--- src/AdminAssistant.Desktop/WPF/AdminAssistant.WPF/MainWindow.xaml.cs
+++ src/AdminAssistant.Desktop/WPF/AdminAssistant.WPF/MainWindow.xaml.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Syncfusion.SfSkinManager;
+using Syncfusion.Windows.Shared;
 
 namespace AdminAssistant.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ChromelessWindow
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            SfSkinManager.ApplyStylesOnApplication = true;
         }
     }
 }
