61342f548c47d867e0a7af5d89e468bf9e03e259
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-07-20 00:00:33 +0100

    Added test to validate CurrencyID is required

--- src/AdminAssistant.Test/Modules/AccountsModule/DomainModel/Validation/BankAccountValidator_UnitTest.cs
+++ src/AdminAssistant.Test/Modules/AccountsModule/DomainModel/Validation/BankAccountValidator_UnitTest.cs
@@ -61,6 +61,24 @@ namespace AdminAssistant.DomainModel.Modules.AccountsModule.Validation
             result.IsValid.Should().BeFalse();
             result.Errors.Should().Contain(x => x.Severity == Severity.Error && x.ErrorCode == "NotEqualValidator" && x.PropertyName == nameof(BankAccount.BankAccountTypeID));
         }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Return_ValidationError_GivenABankAccountWithAMissingCurrencyID()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddAdminAssistantClientSideDomainModel();
+
+            var bankAccount = TestData.BankAccountBuilder.WithTestData().WithCurrencyID(Constants.UnknownRecordID).Build();
+
+            // Act
+            var result = await services.BuildServiceProvider().GetRequiredService<IBankAccountValidator>().ValidateAsync(bankAccount).ConfigureAwait(false);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().Contain(x => x.Severity == Severity.Error && x.ErrorCode == "NotEqualValidator" && x.PropertyName == nameof(BankAccount.CurrencyID));
+        }
     }
 }
 #pragma warning restore CA1707 // Identifiers should not contain underscores
