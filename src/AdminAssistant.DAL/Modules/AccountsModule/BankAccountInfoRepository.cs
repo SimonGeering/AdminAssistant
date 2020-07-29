5c3a2edc4c7757dccdc03de0a5671656e287baed
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-07-29 14:09:56 +0100

    Pulled BankAccountInfo into its own Repository to better work with generic interfaces

---
+++ src/AdminAssistant.DAL/Modules/AccountsModule/BankAccountInfoRepository.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AdminAssistant.DAL.EntityFramework;
+using AdminAssistant.DomainModel.Modules.AccountsModule;
+
+using AutoMapper;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminAssistant.DAL.Modules.AccountsModule
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1812", Justification = "Compiler dosen't understand dependency injection")]
+    internal class BankAccountInfoRepository : RepositoryBase, IBankAccountInfoRepository
+    {
+        public BankAccountInfoRepository(IApplicationDbContext dbContext, IMapper mapper)
+            : base(dbContext, mapper)
+        {
+        }
+
+        public async Task<BankAccountInfo> GetAsync(int id)
+        {
+            // TODO: Check this transform is being done in TSQL server side.
+            return await this.DbContext.BankAccounts.Select(x => new BankAccountInfo
+            {
+                BankAccountID = x.BankAccountID,
+                AccountName = x.AccountName,
+                CurrentBalance = x.CurrentBalance,
+                IsBudgeted = x.IsBudgeted,
+                Symbol = x.Currency.Symbol,
+                DecimalFormat = x.Currency.DecimalFormat,
+            }).FirstOrDefaultAsync(x => x.BankAccountID == id).ConfigureAwait(false);
+        }
+
+        public async Task<IList<BankAccountInfo>> GetListAsync()
+        {
+            // TODO: Check this transform is being done in TSQL server side.
+            return await this.DbContext.BankAccounts.Select(x => new BankAccountInfo
+            {
+                BankAccountID = x.BankAccountID,
+                AccountName = x.AccountName,
+                CurrentBalance = x.CurrentBalance,
+                IsBudgeted = x.IsBudgeted,
+                Symbol = x.Currency.Symbol,
+                DecimalFormat = x.Currency.DecimalFormat,
+            }).ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/AdminAssistant/DAL/Modules/AccountsModule/IBankAccountInfoRepository.cs
@@ -0,0 +1,8 @@
+using AdminAssistant.DomainModel.Modules.AccountsModule;
+
+namespace AdminAssistant.DAL.Modules.AccountsModule
+{
+    public interface IBankAccountInfoRepository : IReadOnlyRepository<BankAccountInfo>
+    {
+    }
+}
