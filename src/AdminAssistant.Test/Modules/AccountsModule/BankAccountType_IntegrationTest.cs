7a92b46e53392f5e71cd53dd57cf69415c01c5e7
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-07-31 12:34:32 +0100

    Pulled common integration test server host setup out into base class.

---
+++ src/AdminAssistant.Test/Modules/AccountsModule/BankAccountType_IntegrationTest.cs
@@ -0,0 +1,44 @@
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+
+using AdminAssistant.DAL.Modules.AccountsModule;
+using AdminAssistant.DomainModel.Modules.AccountsModule;
+using AdminAssistant.WebAPI.v1;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+using System;
+
+namespace AdminAssistant.Modules.AccountsModule
+{
+ 
+    public class BankAccountType_Should : IntegrationTestBase
+    {
+        protected override Action<IServiceCollection> ConfigureTestServices() => services =>
+        {
+            var result = Task.FromResult(new List<BankAccountType>() { new BankAccountType() { BankAccountTypeID = 10, Description = "Test Type" } });
+
+            var mockBankAccountTypeRepository = new Mock<IBankAccountTypeRepository>();
+            mockBankAccountTypeRepository.Setup(x => x.GetListAsync()).Returns(result);
+
+            services.AddSingleton(mockBankAccountTypeRepository.Object);
+        };
+
+        [Fact]
+        [Trait("Category", "Integration")]
+        public async Task ReturnAListOfBankAccountType_GivenACallToBankAccountTypeGet()
+        {
+            // Arrange
+
+            // Act
+            var response = await this.HttpClient.GetFromJsonAsync<BankAccountTypeResponseDto[]>("api/v1/accounts/BankAccountType").ConfigureAwait(false);
+
+            // Assert
+            response.Should().NotBeEmpty();
+        }
+    }
+}
+#pragma warning restore CA1707 // Identifiers should not contain underscores
