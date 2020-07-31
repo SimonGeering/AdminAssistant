9acf9ec2fd8adda4fa490706f1c4cecf62798bdd
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-07-31 13:34:18 +0100

    Split out framework DI into client and server side to allow for the absence of HttpClient on server side.

--- src/Framework/AdminAssistant.Framework/DependencyInjectionExtensions.cs
+++ src/Framework/AdminAssistant.Framework/DependencyInjectionExtensions.cs
@@ -4,10 +4,21 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DependencyInjectionExtensions
     {
-        public static void AddFrameworkServices(this IServiceCollection services)
+        public static void AddClientFrameworkServices(this IServiceCollection services)
+        {
+            AddFrameworkServices(services);
+
+            services.AddTransient<IHttpClientProvider, HttpClientProvider>();
+        }
+
+        public static void AddServerFrameworkServices(this IServiceCollection services)
+        {
+            AddFrameworkServices(services);
+        }
+
+        private static void AddFrameworkServices(this IServiceCollection services)
         {
             services.AddTransient<IDateTimeProvider, DateTimeProvider>();
-            services.AddTransient<IHttpClientProvider, HttpClientProvider>();            
             services.AddTransient<ILoggingProvider, LoggingProvider>();
         }
     }
