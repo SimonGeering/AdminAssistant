209424e2d2a19da6b9aa5584ef1b8c3dd0b5558e
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-02 14:39:29 +0100

    Fixed field mapping to DTO

--- src/AdminAssistant.WebAPI/v1/BankAccountResponseDto.cs
+++ src/AdminAssistant.WebAPI/v1/BankAccountResponseDto.cs
@@ -2,15 +2,18 @@ using System;
 using AdminAssistant.DomainModel.Modules.AccountsModule;
 using AdminAssistant.Framework.TypeMapping;
 
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace AdminAssistant.WebAPI.v1
 {
     public class BankAccountResponseDto : IMapFrom<BankAccount>, IMapTo<BankAccount>
     {
+        [SwaggerSchema("The BankAccount identifier.", ReadOnly = true)]
         public int BankAccountID { get; set; }
+        [SwaggerSchema("The BankAccountType for this BankAccount.")]
         public int BankAccountTypeID { get; set; }
         public int CurrencyID { get; set; }
         public string AccountName { get; set; } = string.Empty;
-        public int Balance { get; set; }
         public bool IsBudgeted { get; set; }
         public int OpeningBalance { get; set; }
         public int CurrentBalance { get; set; }
--- src/AdminAssistant/DomainModel/Modules/AccountsModule/BankAccount.cs
+++ src/AdminAssistant/DomainModel/Modules/AccountsModule/BankAccount.cs
@@ -10,7 +10,6 @@ namespace AdminAssistant.DomainModel.Modules.AccountsModule
         public int BankAccountTypeID { get; set; } = Constants.UnknownRecordID;
         public int CurrencyID { get; set; }
         public string AccountName { get; set; } = string.Empty;
-        public int Balance { get; set; }
         public bool IsBudgeted { get; set; }
         public int OpeningBalance { get; set; }
         public int CurrentBalance { get; set; }
