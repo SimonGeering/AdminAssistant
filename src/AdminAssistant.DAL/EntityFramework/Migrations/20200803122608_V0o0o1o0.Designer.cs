01530e94656078192a5419cec5328901fc2efa51
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-03 14:52:19 +0100

    V0.0.1.0 DB Migration

--- src/AdminAssistant.DAL/EntityFramework/Migrations/20200803122608_V0o0o1o0.Designer.cs
+++ src/AdminAssistant.DAL/EntityFramework/Migrations/20200803122608_V0o0o1o0.Designer.cs
@@ -4,14 +4,16 @@ using AdminAssistant.DAL.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdminAssistant.DAL.EntityFramework.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200803122608_V0o0o1o0")]
+    partial class V0o0o1o0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
