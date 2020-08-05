0394e51afbde9ba165a383d45d3b80cf1242e9a5
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-05 20:45:52 +0100

    #73 Added unit tests for BankController response codes

--- src/AdminAssistant.Test/WebAPI/v1/Accounts/BankController_UnitTest.cs
+++ src/AdminAssistant.Test/WebAPI/v1/Accounts/BankController_UnitTest.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AdminAssistant.DomainModel.Modules.AccountsModule;
 using AdminAssistant.DomainModel.Modules.AccountsModule.CQRS;
 using Ardalis.Result;
+using AutoMapper;
 using FluentAssertions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +16,39 @@ namespace AdminAssistant.WebAPI.v1.Accounts
 {
     public class BankController_GetBankById_Should
     {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Return_Ok200Bank_GivenAnExistingBankID()
+        {
+            // Arrange
+            var bank = Factory.Bank.WithTestData(10).Build();
+
+            var services = new ServiceCollection();
+            services.AddMockLogging();
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(x => x.Send(It.IsAny<BankByIDQuery>(), It.IsAny<CancellationToken>()))
+                        .Returns(Task.FromResult(Result<Bank>.Success(bank)));
+
+            services.AddTransient((sp) => mockMediator.Object);
+            services.AddTransient<BankController>();
+
+            // Act
+            var response = await services.BuildServiceProvider().GetRequiredService<BankController>().GetBankById(bank.BankID).ConfigureAwait(false);
+
+            // Assert
+            response.Result.Should().BeOfType<OkObjectResult>();
+            response.Value.Should().BeNull();
+
+            var result = (OkObjectResult)response.Result;
+            result.Value.Should().BeAssignableTo<BankResponseDto>();
+
+            var value = ((BankResponseDto)result.Value);
+            value.BankID.Should().Be(bank.BankID);
+            value.BankName.Should().Be(bank.BankName);
+        }
+
         [Fact]
         [Trait("Category", "Unit")]
         public async Task Return_Status404NotFound_GivenANonExistentBankID()
