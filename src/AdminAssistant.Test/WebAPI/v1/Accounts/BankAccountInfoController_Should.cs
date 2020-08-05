44e84f24769fda5f9bd4433cd5e84e956ccab2ed
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-05 10:37:23 +0100

    #31 WIP BankAccountInfo WebAPI Integration tests

--- src/AdminAssistant.Test/WebAPI/v1/Accounts/BankAccountInfoController_Should.cs
+++ src/AdminAssistant.Test/WebAPI/v1/Accounts/BankAccountInfoController_Should.cs
@@ -4,42 +4,42 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-
 using AdminAssistant.DAL.Modules.AccountsModule;
 using AdminAssistant.DomainModel.Modules.AccountsModule;
-
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Moq;
 using Xunit;
 
 namespace AdminAssistant.WebAPI.v1.Accounts
 {
+    [Collection("SequentialDBBackedTests")]
     public class BankAccountInfoController_Should : IntegrationTestBase
     {
-        private readonly Mock<IBankAccountInfoRepository> mockBankAccountInfoRepository = new Mock<IBankAccountInfoRepository>();
-
-        protected override Action<IServiceCollection> ConfigureTestServices() => services =>
-        {
-            var result = new List<BankAccountInfo>() { Factory.BankAccountInfo.WithTestData(10).Build() };
-
-            mockBankAccountInfoRepository.Setup(x => x.GetListAsync()).Returns(Task.FromResult(result));
-
-            services.AddSingleton(mockBankAccountInfoRepository.Object);
-        };
-
-        [Fact]
+        [Fact(Skip="WIP Unit test mapping domain to entity")]
         [Trait("Category", "Integration")]
         public async Task ReturnAListOfBankAccountInfo_GivenACallToBankAccountInfoGet()
         {
             // Arrange
+            await this.ResetDatabaseAsync().ConfigureAwait(false);
+
+            var dal = this.Container.GetService<IBankAccountRepository>();
+
+            var acmeSavingsAccount = Factory.BankAccount
+                .WithTestData()
+                .WithAccountName("Acme Savings Account")
+                .Build();
+
+            await dal.SaveAsync(acmeSavingsAccount).ConfigureAwait(false);
+
+            //await dal.SaveAsync(Factory.BankAccount.WithTestData()
+            //                                       .WithAccountName("Acme Savings Account")
+            //                                       .Build()).ConfigureAwait(false);
 
             // Act
             var response = await this.HttpClient.GetFromJsonAsync<BankAccountInfoResponseDto[]>("api/v1/accounts/BankAccountInfo").ConfigureAwait(false);
 
             // Assert
-            response.Should().NotBeEmpty();
-            mockBankAccountInfoRepository.Verify(x => x.GetListAsync(), Times.Once());
+            response.Should().ContainSingle(x => x.AccountName == acmeSavingsAccount.AccountName);
         }
     }
 }
