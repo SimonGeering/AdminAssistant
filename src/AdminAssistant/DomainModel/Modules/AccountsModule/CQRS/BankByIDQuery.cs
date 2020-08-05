9ae1542410d6b109d0fa0e31701aa25de814cc63
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-05 15:44:36 +0100

    #73 Implemented BankByIDQuery

---
+++ src/AdminAssistant/DomainModel/Modules/AccountsModule/CQRS/BankByIDQuery.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using AdminAssistant.DAL.Modules.AccountsModule;
+using AdminAssistant.Framework.Providers;
+using Ardalis.Result;
+using MediatR;
+
+namespace AdminAssistant.DomainModel.Modules.AccountsModule.CQRS
+{
+    public class BankByIDQuery : IRequest<Result<Bank>>
+    {
+        public BankByIDQuery(int bankID)
+        {
+            this.BankID = bankID;
+        }
+
+        public int BankID { get; private set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1812", Justification = "Compiler dosen't understand dependency injection")]
+        internal class BankByIDHandler : RequestHandlerBase<BankByIDQuery, Result<Bank>>
+        {
+            private readonly IBankRepository bankRepository;
+
+            public BankByIDHandler(IBankRepository bankRepository, ILoggingProvider loggingProvider)
+                : base(loggingProvider)
+            {
+                this.bankRepository = bankRepository;
+            }
+
+            public override async Task<Result<Bank>> Handle(BankByIDQuery request, CancellationToken cancellationToken)
+            {
+                this.Log.Start();
+
+                var result = await bankRepository.GetAsync(request.BankID).ConfigureAwait(false);
+
+                return this.Log.Finish(Result<Bank>.Success(result));
+            }
+        }
+    }
+}
