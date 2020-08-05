559edd8e06ece9ee48d7965936eaf5886c439911
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-05 17:43:03 +0100

    #31 Added unit test for BankAccountByIDQuery

---
+++ src/AdminAssistant.Test/DomainModel/Modules/AccountsModule/CQRS/BankAccountByIDQuery_UnitTest.cs
@@ -0,0 +1,39 @@
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+using System.Threading.Tasks;
+using AdminAssistant.DAL.Modules.AccountsModule;
+using Ardalis.Result;
+using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace AdminAssistant.DomainModel.Modules.AccountsModule.CQRS
+{
+    public class BankAccountByIDQuery_Should
+    {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Return_NotFound_GivenANonExistentBankID()
+        {
+            // Arrange
+            var nonExistentBankAccountID = Constants.UnknownRecordID;
+
+            var services = new ServiceCollection();
+            services.AddMockLogging();
+            services.AddAdminAssistantServerSideDomainModel();
+
+            var mockBankRepository = new Mock<IBankAccountRepository>();
+            mockBankRepository.Setup(x => x.GetAsync(nonExistentBankAccountID)).Returns(Task.FromResult<BankAccount>(null!));
+
+            services.AddTransient((sp) => mockBankRepository.Object);
+
+            // Act
+            var result = await services.BuildServiceProvider().GetRequiredService<IMediator>().Send(new BankAccountByIDQuery(nonExistentBankAccountID)).ConfigureAwait(false);
+
+            // Assert
+            result.Status.Should().Be(ResultStatus.NotFound);
+        }
+    }
+}
+#pragma warning restore CA1707 // Identifiers should not contain underscores
