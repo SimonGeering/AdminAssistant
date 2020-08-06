88ce2eb40b9fd3973568702b6d7d295156ba66ce
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-06 14:43:30 +0100

    #31 Added stub unit test for BankAccountController

---
+++ src/AdminAssistant.Test/WebAPI/v1/Accounts/BankAccountController_UnitTest.cs
@@ -0,0 +1,43 @@
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+using System.Threading;
+using System.Threading.Tasks;
+using AdminAssistant.DomainModel.Modules.AccountsModule;
+using AdminAssistant.DomainModel.Modules.AccountsModule.CQRS;
+using Ardalis.Result;
+using AutoMapper;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace AdminAssistant.WebAPI.v1.Accounts
+{
+    public class BankAccountController_GetBankById_Should
+    {
+        [Fact(Skip = "TODO")]
+        [Trait("Category", "Unit")]
+        public async Task Return_Status404NotFound_GivenANonExistentBankID()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            //services.AddMocksOfExternalDependencies();
+
+            //var mockMediator = new Mock<IMediator>();
+            //mockMediator.Setup(x => x.Send(It.IsAny<BankByIDQuery>(), It.IsAny<CancellationToken>()))
+            //            .Returns(Task.FromResult(Result<Bank>.NotFound()));
+
+            //services.AddTransient((sp) => mockMediator.Object);
+            //services.AddTransient<BankController>();
+
+            // Act
+            var response = await services.BuildServiceProvider().GetRequiredService<BankController>().GetBankById(10).ConfigureAwait(false);
+
+            // Assert
+            //response.Result.Should().BeOfType<NotFoundResult>();
+            //response.Value.Should().BeNull();
+        }
+    }
+}
+#pragma warning restore CA1707 // Identifiers should not contain underscores
