1b41092b6ca77281466333b423ed48788e027f8b
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-07 11:02:11 +0100

    #31 Added unit tests for BankAccountController GetBankAccountById

--- src/AdminAssistant.Test/WebAPI/v1/Accounts/BankAccountController_UnitTest.cs
+++ src/AdminAssistant.Test/WebAPI/v1/Accounts/BankAccountController_UnitTest.cs
@@ -16,28 +16,62 @@ namespace AdminAssistant.WebAPI.v1.Accounts
 {
     public class BankAccountController_GetBankById_Should
     {
-        [Fact(Skip = "TODO")]
+        [Fact]
         [Trait("Category", "Unit")]
-        public async Task Return_Status404NotFound_GivenANonExistentBankID()
+        public async Task Return_Ok200Bank_Given_AnExistingBankAccountID()
+        {
+            // Arrange
+            var bankAccount = Factory.BankAccount.WithTestData(10).Build();
+
+            var services = new ServiceCollection();
+            services.AddMockLogging();
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(x => x.Send(It.IsAny<BankAccountByIDQuery>(), It.IsAny<CancellationToken>()))
+                        .Returns(Task.FromResult(Result<BankAccount>.Success(bankAccount)));
+
+            services.AddTransient((sp) => mockMediator.Object);
+            services.AddTransient<BankAccountController>();
+
+            // Act
+            var response = await services.BuildServiceProvider().GetRequiredService<BankAccountController>().GetBankAccountById(bankAccount.BankAccountID).ConfigureAwait(false);
+
+            // Assert
+            response.Result.Should().BeOfType<OkObjectResult>();
+            response.Value.Should().BeNull();
+
+            var result = (OkObjectResult)response.Result;
+            result.Value.Should().BeAssignableTo<BankAccountResponseDto>();
+
+            var value = ((BankAccountResponseDto)result.Value);
+            value.BankAccountID.Should().Be(bankAccount.BankAccountID);
+            value.AccountName.Should().Be(bankAccount.AccountName);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Return_Status404NotFound_Given_ANonExistentBankAccountID()
         {
             // Arrange
             var services = new ServiceCollection();
-            //services.AddMocksOfExternalDependencies();
+            services.AddMocksOfExternalDependencies();
 
-            //var mockMediator = new Mock<IMediator>();
-            //mockMediator.Setup(x => x.Send(It.IsAny<BankByIDQuery>(), It.IsAny<CancellationToken>()))
-            //            .Returns(Task.FromResult(Result<Bank>.NotFound()));
+            var mockMediator = new Mock<IMediator>();
+            mockMediator.Setup(x => x.Send(It.IsAny<BankAccountByIDQuery>(), It.IsAny<CancellationToken>()))
+                        .Returns(Task.FromResult(Result<BankAccount>.NotFound()));
 
-            //services.AddTransient((sp) => mockMediator.Object);
-            //services.AddTransient<BankController>();
+            services.AddTransient((sp) => mockMediator.Object);
+            services.AddTransient<BankAccountController>();
 
             // Act
-            var response = await services.BuildServiceProvider().GetRequiredService<BankController>().GetBankById(10).ConfigureAwait(false);
+            var response = await services.BuildServiceProvider().GetRequiredService<BankAccountController>().GetBankAccountById(10).ConfigureAwait(false);
 
             // Assert
-            //response.Result.Should().BeOfType<NotFoundResult>();
-            //response.Value.Should().BeNull();
+            response.Result.Should().BeOfType<NotFoundResult>();
+            response.Value.Should().BeNull();
         }
+
     }
 }
 #pragma warning restore CA1707 // Identifiers should not contain underscores
