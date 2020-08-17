ee516c7f749434b51204c58fcd9ed83b7c5af3b2
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-17 11:44:12 +0100

    #31 Added busy spinner

--- src/AdminAssistant/UI/Modules/AccountsModule/BankAccountEditDialog/IBankAccountEditDialogViewModel.cs
+++ src/AdminAssistant/UI/Modules/AccountsModule/BankAccountEditDialog/IBankAccountEditDialogViewModel.cs
@@ -1,7 +1,5 @@
 using System;
 using System.ComponentModel;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using AdminAssistant.DomainModel.Modules.AccountsModule;
 using AsyncAwaitBestPractices.MVVM;
 
