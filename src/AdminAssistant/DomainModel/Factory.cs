8dd5c1e1760dec8f63d2be28b1d6c9315291ccec
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-30 15:39:35 +0100

    #125 Merged budget domain factory to root factory

--- src/AdminAssistant/DomainModel/Factory.cs
+++ src/AdminAssistant/DomainModel/Factory.cs
@@ -1,16 +1,24 @@
 using AdminAssistant.DomainModel.Modules.AccountsModule.Builders;
+using AdminAssistant.DomainModel.Modules.BudgetModule.Builders;
 using AdminAssistant.DomainModel.Modules.DocumentsModule.Builders;
 
 namespace AdminAssistant.DomainModel
 {
     public static class Factory
     {
+        // AccountsModule ...
         public static IBankBuilder Bank => new BankBuilder();
         public static IBankAccountBuilder BankAccount => new BankAccountBuilder();
         public static IBankAccountInfoBuilder BankAccountInfo => new BankAccountInfoBuilder();
         public static IBankAccountTypeBuilder BankAccountType => new BankAccountTypeBuilder();
-        public static ICurrencyBuilder Currency => new CurrencyBuilder();
-        public static IDocumentBuilder Document => new DocumentBuilder();
         public static IBankAccountTransactionBuilder BankAccountTransaction => new BankAccountTransactionBuilder();
+        public static ICurrencyBuilder Currency => new CurrencyBuilder();
+
+        // BudgetModule ...
+        public static IBudgetBuilder Budget => new BudgetBuilder();
+
+        // DocumentsModule ...
+        public static IDocumentBuilder Document => new DocumentBuilder();
+
     }
 }
--- src/AdminAssistant/DomainModel/Modules/BudgetModule/Factory.cs
+++
@@ -1,9 +0,0 @@
-using AdminAssistant.DomainModel.Modules.BudgetModule.Builders;
-
-namespace AdminAssistant.DomainModel.Modules.BudgetModule
-{
-    public static class Factory
-    {
-        public static IBudgetBuilder Budget => new BudgetBuilder();
-    }
-}
