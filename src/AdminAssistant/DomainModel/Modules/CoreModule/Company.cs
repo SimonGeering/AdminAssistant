b1634b8013d37dd93cee6af585d9cc39492eed91
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-12-17 11:47:30 +0000

    #146 Switched Company to be a record

--- src/AdminAssistant/DomainModel/Modules/CoreModule/Company.cs
+++ src/AdminAssistant/DomainModel/Modules/CoreModule/Company.cs
@@ -1,7 +1,7 @@
 namespace AdminAssistant.DomainModel.Modules.CoreModule
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "WIP Code")]
-    public class Company
+    public record Company
     {
         public const int CompanyNumberMaxLength = 50;
         public const int VATNumberMaxLength = 50;
