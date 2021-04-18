437c9afe2fa1cd444e42e10399bcd7385f5d1778
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2021-04-18 21:05:42 +0100

    #171 - Switch BankAccountType WebAPI Request / Response DTOs to be records

--- src/AdminAssistant.Blazor/Server/WebAPI/v1/AccountsModule/BankAccountTypeController.BankAccountTypeResponseDto.cs
+++ src/AdminAssistant.Blazor/Server/WebAPI/v1/AccountsModule/BankAccountTypeController.BankAccountTypeResponseDto.cs
@@ -3,9 +3,9 @@ using AdminAssistant.Framework.TypeMapping;
 
 namespace AdminAssistant.WebAPI.v1.AccountsModule
 {
-    public class BankAccountTypeResponseDto : IMapFrom<BankAccountType>
+    public record BankAccountTypeResponseDto : IMapFrom<BankAccountType>
     {
-        public int BankAccountTypeID { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public int BankAccountTypeID { get; init; }
+        public string Description { get; init; } = string.Empty;
     }
 }
