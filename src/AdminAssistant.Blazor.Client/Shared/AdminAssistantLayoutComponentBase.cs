0ae59d81f8038a5814434b525795408848318a29
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2021-09-22 23:19:28 +0100

    Convert to file based namespaces

--- src/AdminAssistant.Blazor.Client/Shared/AdminAssistantLayoutComponentBase.cs
+++ src/AdminAssistant.Blazor.Client/Shared/AdminAssistantLayoutComponentBase.cs
@@ -2,33 +2,32 @@ using AdminAssistant.UI;
 using Microsoft.AspNetCore.Components;
 using Syncfusion.Blazor.Spinner;
 
-namespace AdminAssistant.Blazor.Client.Shared
+namespace AdminAssistant.Blazor.Client.Shared;
+
+public abstract class AdminAssistantLayoutComponentBase<TViewModel> : LayoutComponentBase
+    where TViewModel : IViewModelBase
 {
-    public abstract class AdminAssistantLayoutComponentBase<TViewModel> : LayoutComponentBase
-        where TViewModel : IViewModelBase
-    {
 #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
-        [Inject]
-        protected TViewModel vm { get; set; }
+    [Inject]
+    protected TViewModel vm { get; set; }
 #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
-        protected SfSpinner SfSpinner { get; set; } = new SfSpinner();
+    protected SfSpinner SfSpinner { get; set; } = new SfSpinner();
 
-        protected override void OnInitialized()
+    protected override void OnInitialized()
+    {
+        vm.PropertyChanged += (o, e) => StateHasChanged();
+        vm.IsBusyChanged += (sender, isBusy) =>
         {
-            vm.PropertyChanged += (o, e) => StateHasChanged();
-            vm.IsBusyChanged += (sender, isBusy) =>
-            {
-                if (isBusy)
-                    SfSpinner.ShowAsync();
-                else
-                    SfSpinner.HideAsync();
-            };
-            base.OnInitialized();
-        }
-
-        protected override Task OnAfterRenderAsync(bool firstRender) => base.OnAfterRenderAsync(firstRender);
-
-        protected override Task OnInitializedAsync() => vm.OnInitializedAsync();
+            if (isBusy)
+                SfSpinner.ShowAsync();
+            else
+                SfSpinner.HideAsync();
+        };
+        base.OnInitialized();
     }
+
+    protected override Task OnAfterRenderAsync(bool firstRender) => base.OnAfterRenderAsync(firstRender);
+
+    protected override Task OnInitializedAsync() => vm.OnInitializedAsync();
 }
