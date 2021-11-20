c9d7d42b32820d70bd25a2fb103e4f86f13e1880
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2021-11-20 23:05:05 +0000

    WIP BankAccount Get integration test.

--- src/AdminAssistant.Test/WebAPI/v1/AccountsModule/BankAccountController_IntegrationTest.cs
+++ src/AdminAssistant.Test/WebAPI/v1/AccountsModule/BankAccountController_IntegrationTest.cs
@@ -1,24 +1,37 @@
 #if DEBUG // quick and dirty fix for #85 category filtering breaking CI Unit Test run.
 #pragma warning disable CA1707 // Identifiers should not contain underscores
 
+using AdminAssistant.DomainModel.Modules.AccountsModule;
+using AdminAssistant.Infra.DAL.Modules.AccountsModule;
+using AdminAssistant.UI.Shared.WebAPIClient.v1;
+
 namespace AdminAssistant.Test.WebAPI.v1.AccountsModule;
 
 [Collection("SequentialDBBackedTests")]
 public class BankAccount_Get_Should : IntegrationTestBase
 {
-    //[Fact(Skip = "WIP")]
+    //[Fact]
     //[Trait("Category", "Integration")]
     //public async Task Return_ABankAccount_Given_BankAccountID()
     //{
     //    // Arrange
-    //    int bankAccountID = 10;
-    //    await this.ResetDatabaseAsync().ConfigureAwait(false);
+    //    await ResetDatabaseAsync().ConfigureAwait(false);
+
+    //    var dal = Container.GetRequiredService<IBankAccountRepository>();
+    //    await dal.SaveAsync(new BankAccount()
+    //    {
+    //        BankAccountTypeID = 10,
+    //        CurrencyID = 10,
+    //        AccountName = "Acme Bank PLC"
+    //    }).ConfigureAwait(false);
+    //    var acmeBuildingSocietyAccount = await dal.SaveAsync(new BankAccount() { AccountName = "Acme Building Society Account" }).ConfigureAwait(false);
 
     //    // Act
-    //    var response = await this.Container.GetRequiredService<IAdminAssistantWebAPIClient>().GetBankAccountByIdAsync(bankAccountID).ConfigureAwait(false);
+    //    var response = await Container.GetRequiredService<IAdminAssistantWebAPIClient>().GetBankAccountByIdAsync(acmeBuildingSocietyAccount.BankAccountID).ConfigureAwait(false);
 
     //    // Assert
-    //    response.BankAccountID.Should().Be(bankAccountID);
+    //    response.BankAccountID.Should().Be(acmeBuildingSocietyAccount.BankAccountID);
+    //    response.AccountName.Should().Be(acmeBuildingSocietyAccount.AccountName);
     //}
 }
 #pragma warning restore CA1707 // Identifiers should not contain underscores
