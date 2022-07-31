145b5d630ecd67eac490af5968cf0b1066606e2a
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2022-07-31 22:43:11 +0100

    WIP Adding opentelemetry

--- src/Services/AdminAssistant.Core.API/Program.cs
+++ src/Services/AdminAssistant.Core.API/Program.cs
@@ -1,29 +1,54 @@
-using AdminAssistant;
+#pragma warning disable IDE0053 // Use expression body for lambda expressions
 using AdminAssistant.Core.API;
-using AdminAssistant.DomainModel.Modules.CoreModule;
-using AdminAssistant.DomainModel.Modules.CoreModule.CQRS;
 using AdminAssistant.DomainModel.Shared;
-using AdminAssistant.Infra.Providers;
-using Ardalis.Result;
-using AutoMapper;
 using HealthChecks.UI.Client;
-using MediatR;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Swashbuckle.AspNetCore.Annotations;
+//using OpenTelemetry.Metrics;
+//using OpenTelemetry.Resources;
+//using OpenTelemetry.Trace;
+
+//const string SourceName = "AdminAssistant.Core.API";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHealthChecks();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+builder.Services.AddSwaggerGen(options =>
 {
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     // See https://github.com/domaindrivendev/Swashbuckle.AspNetCore for an overview of options available here.
 
     // Include documentation from Annotations (Swashbuckle.AspNetCore.Annotations)...
-    c.EnableAnnotations(); // https://github.com/domaindrivendev/Swashbuckle.AspNetCore#install-and-enable-annotations
+    options.EnableAnnotations(); // https://github.com/domaindrivendev/Swashbuckle.AspNetCore#install-and-enable-annotations
 });
+//builder.Services.AddOpenTelemetryTracing(config =>
+//{
+//    config.AddSource(SourceName);
+//    config.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(SourceName).AddTelemetrySdk());
+//    config.AddSqlClientInstrumentation(options =>
+//    {
+//        options.SetDbStatementForText = true;
+//        options.RecordException = true;
+//    });
+//    config.AddAspNetCoreInstrumentation(options =>
+//    {
+//        options.Filter = (req) => !req.Request.Path.ToUriComponent().Contains("index.html", StringComparison.OrdinalIgnoreCase)
+//                                          && !req.Request.Path.ToUriComponent().Contains("swagger", StringComparison.OrdinalIgnoreCase);
+//    });
+//    config.AddHttpClientInstrumentation();
+//    config.AddOtlpExporter(otlpOptions =>
+//    {
+//        otlpOptions.Endpoint = new Uri("http://adminassistant.opentelemetry.collector");
+//    });
+//});
+//builder.Services.AddOpenTelemetryMetrics(builder =>
+//{
+//    builder.AddHttpClientInstrumentation();
+//    builder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(SourceName).AddTelemetrySdk());
+
+//});
+
 builder.Services.AddAutoMapper(typeof(AdminAssistant.Infra.DAL.MappingProfile), typeof(MappingProfile));
 
 builder.Services.AddAdminAssistantServerSideProviders();
@@ -49,3 +74,5 @@ app.UseHttpsRedirection();
 app.MapCurrencyEndpoint();
 
 app.Run();
+
+#pragma warning restore IDE0053 // Use expression body for lambda expressions
