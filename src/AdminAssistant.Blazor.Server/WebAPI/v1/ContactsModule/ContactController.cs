ff6e38f7263f5941a7469e8a90994c664470d30c
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2022-12-21 13:13:33 +0000

    WIP adding ContactGetById

--- src/AdminAssistant.Blazor.Server/WebAPI/v1/ContactsModule/ContactController.cs
+++ src/AdminAssistant.Blazor.Server/WebAPI/v1/ContactsModule/ContactController.cs
@@ -1,8 +1,10 @@
+using System;
 using AdminAssistant.DomainModel.Modules.ContactsModule.CQRS;
 using AdminAssistant.Infra.Providers;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace AdminAssistant.WebAPI.v1.ContactsModule;
@@ -28,5 +30,17 @@ public class ContactController : WebAPIControllerBase
         var response = Mapper.Map<IEnumerable<ContactResponseDto>>(result.Value);
 
         return Log.Finish(Ok(response));
+    }
+
+    [HttpGet("{contactID}", Name = nameof(ContactGetById))]
+    [SwaggerOperation("Gets the Contact with the given ID.", OperationId = "GetContactById")]
+    [SwaggerResponse(StatusCodes.Status200OK, "OK - returns the Contact requested.", type: typeof(ContactResponseDto))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "BadRequest - When the given ContactID is invalid.")]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "NotFound - When the given ContactID does not exist.")]
+    public async Task<ActionResult<ContactResponseDto>> ContactGetById([SwaggerParameter("The ID of the Contact to be returned.", Required = true)] int contactId)
+    {
+        // WIP
+        //return BadRequest(new ContactResponseDto());
+        return Ok(new ContactResponseDto());
     }
 }
