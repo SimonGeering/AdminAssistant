f0413cd14613e9c456feec6246ebce263d091e56
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-05-19 19:25:57 +0100

    Added TaskEntity stub

--- src/AdminAssistant.Infra/DAL/EntityFramework/Model/Tasks/TaskEntity.cs
+++ src/AdminAssistant.Infra/DAL/EntityFramework/Model/Tasks/TaskEntity.cs
@@ -1,13 +1,12 @@
-/*
-Table "Tasks.Task" 
-{
-  "TaskID" INT [pk]
-  "AuditID" INT
-  "OwnerID" INT
-}
-*/
 namespace AdminAssistant.Infra.DAL.EntityFramework.Model.Tasks;
 
 public sealed class TaskEntity
 {
+    // Table "Tasks.Task"
+    public int TaskID { get; set; } // PK
+    public int AuditID { get; set; }
+    public int OwnerID { get; internal set; }
+
+    public Core.AuditEntity Audit { get; internal set; } = null!;
+    public Core.OwnerEntity Owner { get; internal set; } = null!;
 }
