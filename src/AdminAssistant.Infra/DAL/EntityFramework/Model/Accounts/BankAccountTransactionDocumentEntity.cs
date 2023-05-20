de0d2ec22b924f98abc6c92b726555130841eaf3
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-05-20 01:29:10 +0100

    Stubbed out BankAccountTransactionDocumentEntity

--- src/AdminAssistant.Infra/DAL/EntityFramework/Model/Accounts/BankAccountTransactionDocumentEntity.cs
+++ src/AdminAssistant.Infra/DAL/EntityFramework/Model/Accounts/BankAccountTransactionDocumentEntity.cs
@@ -1,16 +1,12 @@
-/*
-Table "Accounts.BankAccountTransactionDocument" 
-{
-  "BankAccountTransactionDocumentID" INT[pk]
-  "AuditID" INT
-  "BankAccountTransactionID" INT
-  "DocumentID" INT
-}
-Ref: "Accounts.BankAccountTransaction"."BankAccountTransactionID" < "Accounts.BankAccountTransactionDocument"."BankAccountTransactionDocumentID"
-
-*/
 namespace AdminAssistant.Infra.DAL.EntityFramework.Model.Accounts;
 
 public sealed class BankAccountTransactionDocumentEntity
 {
+    // Table "Accounts.BankAccountTransactionDocument" 
+    public int BankAccountTransactionDocumentID { get; set; } // PK
+    public int BankAccountTransactionID { get; set; }
+    public int DocumentID { get; set; }
+    public int AuditID { get; internal set; }
+    public int PayeeID { get; internal set; }
+    // Ref: "Accounts.BankAccountTransaction"."BankAccountTransactionID" < "Accounts.BankAccountTransactionDocument"."BankAccountTransactionDocumentID"
 }
