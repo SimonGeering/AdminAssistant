ee94020d2bffea5f145efa6b4e2f4ca0e0790f9a
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-11-18 21:38:15 +0000

    Fixed https://github.com/dotnet/docs/issues/34893

--- src/AdminAssistant.Abstractions/Framework/Configuration/ConfigurationException.cs
+++ src/AdminAssistant.Abstractions/Framework/Configuration/ConfigurationException.cs
@@ -1,19 +1,4 @@
-using System.Runtime.Serialization;
-
 namespace AdminAssistant.Framework.Configuration;
 
-[Serializable]
-public class ConfigurationException : ApplicationBaseException
-{
-    public ConfigurationException(string? message) : base(message)
-    {
-    }
-
-    protected ConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
-
-    public ConfigurationException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
-}
+public class ConfigurationException(string? message)
+    : ApplicationBaseException(message) { }
--- src/AdminAssistant.Abstractions/Framework/TypeMapping/TypeMappingException.cs
+++ src/AdminAssistant.Abstractions/Framework/TypeMapping/TypeMappingException.cs
@@ -2,18 +2,5 @@ using System.Runtime.Serialization;
 
 namespace AdminAssistant.Framework.TypeMapping;
 
-[Serializable]
-public class TypeMappingException : ApplicationBaseException
-{
-    public TypeMappingException(string? message) : base(message)
-    {
-    }
-
-    protected TypeMappingException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
-
-    public TypeMappingException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
-}
+public class TypeMappingException(string? message)
+    : ApplicationBaseException(message) { }
