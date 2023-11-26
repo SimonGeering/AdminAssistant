326271b7ce4088524aa01d4822e79a9975556c5d
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-11-26 20:25:55 +0000

    Added AdminAssistant.Infra.WebApiClient

---
+++ src/AdminAssistant.Infra.WebAPIClient/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AdminAssistant.DomainModel.Shared;
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddAdminAssistantWebAPIClient(this IServiceCollection services, ConfigurationSettings configurationSettings)
+        => AddAdminAssistantWebAPIClient(services, new Uri(configurationSettings.WebApiClientBaseAddress));
+
+    public static void AddAdminAssistantWebAPIClient(this IServiceCollection services, Uri baseAddress)
+    {
+        //services.AddHttpClient<IAdminAssistantWebAPIClient, AdminAssistantWebAPIClient>(AdminAssistant.Constants.AdminAssistantWebAPIClient, (httpClient) => httpClient.BaseAddress = baseAddress);
+        //services.AddAutoMapper(typeof(MappingProfile));
+    }
+}
