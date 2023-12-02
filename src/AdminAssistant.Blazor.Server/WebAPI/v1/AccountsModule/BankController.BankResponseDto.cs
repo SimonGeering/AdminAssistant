6e9469e29cc07f50ae8575213764f9b554220303
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-12-02 21:34:32 +0000

    Fixed tests following restructure

--- src/AdminAssistant.Blazor.Server/WebAPI/v1/AccountsModule/BankController.BankResponseDto.cs
+++ src/AdminAssistant.Blazor.Server/WebAPI/v1/AccountsModule/BankController.BankResponseDto.cs
@@ -6,4 +6,8 @@ public sealed record BankResponseDto : IMapFrom<Bank>
 {
     public int BankID { get; init; } = Constants.UnknownRecordID;
     public string BankName { get; init; } = string.Empty;
+
+    public void MapFrom(AutoMapper.Profile profile) => profile
+        .CreateMap<Bank, BankResponseDto>()
+        .ForMember(x => x.BankName, opt => opt.MapFrom(s => s.BankName.Value));
 }
