80795a62e0145d4cc06d7778ccaef00159f0097b
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2023-12-10 19:24:07 +0000

    WIP Aspire

--- src/AdminAssistant.Aspire.ServiceDefaults/Extensions.cs
+++ src/AdminAssistant.Aspire.ServiceDefaults/Extensions.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
@@ -14,11 +15,8 @@ public static class Extensions
     public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
     {
         builder.ConfigureOpenTelemetry();
-
         builder.AddDefaultHealthChecks();
-
         builder.Services.AddServiceDiscovery();
-
         builder.Services.ConfigureHttpClientDefaults(http =>
         {
             // Turn on resilience by default
@@ -27,7 +25,6 @@ public static class Extensions
             // Turn on service discovery by default
             http.UseServiceDiscovery();
         });
-
         return builder;
     }
 
@@ -40,11 +37,7 @@ public static class Extensions
         });
 
         builder.Services.AddOpenTelemetry()
-            .WithMetrics(metrics =>
-            {
-                metrics.AddRuntimeInstrumentation()
-                       .AddBuiltInMeters();
-            })
+            .WithMetrics(metrics => metrics.AddRuntimeInstrumentation().AddBuiltInMeters())
             .WithTracing(tracing =>
             {
                 if (builder.Environment.IsDevelopment())
@@ -56,7 +49,10 @@ public static class Extensions
                 tracing.AddAspNetCoreInstrumentation()
                        .AddGrpcClientInstrumentation()
                        .AddHttpClientInstrumentation()
-                       .AddSqlClientInstrumentation();
+                       .AddEntityFrameworkCoreInstrumentation()
+                       .AddNpgsql()
+                       .AddSqlClientInstrumentation()
+                       .AddConsoleExporter();
             });
 
         builder.AddOpenTelemetryExporters();
