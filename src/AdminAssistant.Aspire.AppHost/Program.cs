6f44b1a015e26930a741b24298aedecbb30a0fce
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2024-04-07 22:08:08 +0100

    Initial Aspire projects building

--- src/AdminAssistant.Aspire.AppHost/Program.cs
+++ src/AdminAssistant.Aspire.AppHost/Program.cs
@@ -1,4 +1,8 @@
-using AdminAssistant.Infrastructure.EntityFramework;
+// ReSharper disable UnusedVariable
+#pragma warning disable S1481
+
+// https://devblogs.microsoft.com/dotnet/announcing-dotnet-aspire-preview-2/
+// https://learn.microsoft.com/en-gb/dotnet/aspire/
 using AdminAssistant.Shared;
 using Ardalis.GuardClauses;
 using Microsoft.Extensions.Configuration;
@@ -6,47 +10,80 @@ using SimonGeering.Framework.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Config Settings ...
 var configurationSettings = builder.Configuration.GetSection(nameof(ConfigurationSettings)).Get<ConfigurationSettings>();
 Guard.Against.Null(configurationSettings);
 
 if (Enum.TryParse(configurationSettings.DatabaseProvider, out DatabaseProvider databaseProvider) == false)
     throw new ConfigurationException("Unable to load 'DatabaseProvider' configuration setting.");
 
-//var webApp = builder.AddProject<Projects.AdminAssistant_Blazor_Server>("AdminAssistant.Blazor.Server");
+// Main App ...
+var webApp = builder.AddProject<Projects.AdminAssistant_Blazor_Server>(configurationSettings.AspireServerAppName);
 
-//switch (databaseProvider)
-//{
-//    case DatabaseProvider.SQLServer:
-//        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
-//        var sqlServerExternalDb = builder.AddSqlServerConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
-//        webApp.WithReference(sqlServerExternalDb);
-//        break;
+// Database Server ...
+switch (databaseProvider)
+{
+    case DatabaseProvider.SQLServer:
+        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
+        throw new NotImplementedException("TODO - DatabaseProvider.SQLServer");
+        // var sqlServerExternalDb = builder.AddSqlServerConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
+        // webApp.WithReference(sqlServerExternalDb);
+        // break;
 
-//    case DatabaseProvider.SQLServerLocalDB:
-//        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
-//        var sqlServerLocalDb = builder.AddSqlServerConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
-//        webApp.WithReference(sqlServerLocalDb);
-//        break;
+    case DatabaseProvider.SQLServerLocalDB:
+        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
+        throw new NotImplementedException("TODO - DatabaseProvider.SQLServerLocalDB");
+        // var sqlServerLocalDb = builder.AddSqlServerConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
+        // webApp.WithReference(sqlServerLocalDb);
+        // break;
 
-//    case DatabaseProvider.SQLite:
-//        // Unsupported.
-//        break;
+    case DatabaseProvider.SQLite:
+        throw new System.NotSupportedException("SQLite database provider not currently supported.");
 
-//    case DatabaseProvider.PostgresSQL:
-//        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
-//        var postgresExternalDb = builder.AddPostgresConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
-//        webApp.WithReference(postgresExternalDb);
-//        break;
+    case DatabaseProvider.PostgresSQL:
+        Guard.Against.NullOrEmpty(configurationSettings.ConnectionString);
+        throw new NotImplementedException("TODO - DatabaseProvider.PostgresSQL");
+        // var postgresExternalDb = builder.AddPostgresConnection(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName, configurationSettings.ConnectionString);
+        // webApp.WithReference(postgresExternalDb);
+        // break;
 
-//    case DatabaseProvider.SQLServerContainer:
-//        var sqlServerDbContainer = builder.AddSqlServerContainer("AdminAssistant_SQLServer").AddDatabase(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName);
-//        webApp.WithReference(sqlServerDbContainer);
-//        break;
+    case DatabaseProvider.SQLServerContainer:
+        var sqlServerDbContainer = builder
+            .AddSqlServer(configurationSettings.AspireSqlServerName)
+            .AddDatabase(configurationSettings.AspireDatabaseName);
+        webApp.WithReference(sqlServerDbContainer);
+        break;
 
-//    case DatabaseProvider.PostgresSQLContainer:
-//        var postgresDbContainer = builder.AddPostgresContainer("AdminAssistant_Postgres").AddDatabase(ConfigurationSettings.AdminAssistantWellKnownConnectionStringName);
-//        webApp.WithReference(postgresDbContainer);
-//        break;
-//}
+    case DatabaseProvider.PostgresSQLContainer:
+        var postgresDbContainer = builder
+            .AddPostgres(configurationSettings.AspirePostgresServerName)
+            .WithPgAdmin()
+            .AddDatabase(configurationSettings.AspireDatabaseName);
+        webApp.WithReference(postgresDbContainer);
+        break;
+}
+
+// Services ...
+var accounts = builder.AddProject<Projects.AdminAssistant_Services_Accounts>("AdminAssistant-AccountsApi");
+var admin = builder.AddProject<Projects.AdminAssistant_Services_Admin>("AdminAssistant-AdminApi");
+var assetRegister = builder.AddProject<Projects.AdminAssistant_Services_AssetRegister>("AdminAssistant-AssetRegisterApi");
+var budget = builder.AddProject<Projects.AdminAssistant_Services_Budget>("AdminAssistant-BudgetApi");
+var calendar = builder.AddProject<Projects.AdminAssistant_Services_Calendar>("AdminAssistant-CalendarApi");
+var contacts = builder.AddProject<Projects.AdminAssistant_Services_Contacts>("AdminAssistant-ContactsApi");
+var core = builder.AddProject<Projects.AdminAssistant_Services_Core>("AdminAssistant-CoreApi");
+var documents = builder.AddProject<Projects.AdminAssistant_Services_Documents>("AdminAssistant-DocumentsApi");
+var mail = builder.AddProject<Projects.AdminAssistant_Services_Mail>("AdminAssistant-MailApi");
+var notes = builder.AddProject<Projects.AdminAssistant_Services_Notes>("AdminAssistant-NotesApi");
+var scheduledPayments = builder.AddProject<Projects.AdminAssistant_Services_ScheduledPayments>("AdminAssistant-ScheduledPaymentsApi");
+var tasks = builder.AddProject<Projects.AdminAssistant_Services_Tasks>("AdminAssistant-TasksApi");
+
+// Background Job Host ...
+var backgroundJobHost = builder.AddProject<Projects.AdminAssistant_Hangfire>("AdminAssistant-HangFire");
+
+// Gateway ...
+var gateway = builder.AddProject<Projects.AdminAssistant_Gateway>("AdminAssistant-Gateway");
+
+webApp.WithReference(gateway);
 
 builder.Build().Run();
+#pragma warning restore S1481
--- src/AdminAssistant.DomainModel/Shared/ConfigurationSettings.cs
+++ src/AdminAssistant.DomainModel/Shared/ConfigurationSettings.cs
@@ -2,8 +2,13 @@ namespace AdminAssistant.Shared;
 
 public sealed record ConfigurationSettings
 {
-    public const string AdminAssistantWellKnownConnectionStringName = "AdminAssistant";
     public bool UseAspire { get; set; } = true;
+    public string AspireServerAppName { get; set; } = "AdminAssistantServer";
+    public string AspireSqlServerName { get; set; } = "AdminAssistantDatabaseServer";
+    public string AspirePostgresServerName { get; set; } = "AdminAssistantDatabaseServer";
+    public string AspireDatabaseName { get; set; } = "AdminAssistant";
+
+
     public string WebApiClientBaseAddress { get; set; } = "https://localhost:5001";
     public string DatabaseProvider { get; set; } = string.Empty;
     public string ConnectionString { get; set; } = string.Empty;
