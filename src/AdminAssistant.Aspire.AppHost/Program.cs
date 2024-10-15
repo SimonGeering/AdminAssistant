fde77ac97c2be7e6b52b61798d32eb8df528ac99
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2024-10-15 22:50:01 +0100

    upgrade to aspire 9

--- src/AdminAssistant.Aspire.AppHost/Program.cs
+++ src/AdminAssistant.Aspire.AppHost/Program.cs
@@ -8,16 +8,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Database Server ...
 var sqlServer = builder
-    .AddSqlServer(Constants.DatabaseServerName)
-    .WithHealthCheck();
+    .AddSqlServer(Constants.DatabaseServerName);
 
-var applicationDatabase = sqlServer.AddDatabase(Constants.ApplicationDatabaseName)
-    .WithHealthCheck()
-    .WaitFor(sqlServer);
-
-var scheduledJobDatabase = sqlServer.AddDatabase(Constants.ScheduledJobDatabase)
-    .WithHealthCheck()
-    .WaitFor(sqlServer);
+var applicationDatabase = sqlServer.AddDatabase(Constants.ApplicationDatabaseName);
+var scheduledJobDatabase = sqlServer.AddDatabase(Constants.ScheduledJobDatabase);
 
 var databaseMigrationWorkerService = builder.AddProject<Projects.AdminAssistant_Aspire_DatabaseMigrationWorkerService>(Constants.Services.DatabaseMigrationWorkerService)
     .WithReference(applicationDatabase)
@@ -26,62 +20,50 @@ var databaseMigrationWorkerService = builder.AddProject<Projects.AdminAssistant_
 // Services ...
 var accounts = builder.AddProject<Projects.AdminAssistant_Services_Accounts>(Constants.Services.AccountsApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var admin = builder.AddProject<Projects.AdminAssistant_Services_Admin>(Constants.Services.AdminApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var assetRegister = builder.AddProject<Projects.AdminAssistant_Services_AssetRegister>(Constants.Services.AssetRegisterApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var budget = builder.AddProject<Projects.AdminAssistant_Services_Budget>(Constants.Services.BudgetApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var calendar = builder.AddProject<Projects.AdminAssistant_Services_Calendar>(Constants.Services.CalendarApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var contacts = builder.AddProject<Projects.AdminAssistant_Services_Contacts>(Constants.Services.ContactsApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var core = builder.AddProject<Projects.AdminAssistant_Services_Core>(Constants.Services.CoreApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var documents = builder.AddProject<Projects.AdminAssistant_Services_Documents>(Constants.Services.DocumentsApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var mail = builder.AddProject<Projects.AdminAssistant_Services_Mail>(Constants.Services.MailApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var notes = builder.AddProject<Projects.AdminAssistant_Services_Notes>(Constants.Services.NotesApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var scheduledPayments = builder.AddProject<Projects.AdminAssistant_Services_ScheduledPayments>(Constants.Services.ScheduledPaymentsApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 var tasks = builder.AddProject<Projects.AdminAssistant_Services_Tasks>(Constants.Services.TasksApi)
     .WithReference(applicationDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
 // API Gateway ...
@@ -98,23 +80,19 @@ var gateway = builder.AddProject<Projects.AdminAssistant_Gateway>(Constants.Serv
     .WithReference(notes).WaitFor(notes)
     .WithReference(scheduledPayments).WaitFor(scheduledPayments)
     .WithReference(tasks).WaitFor(tasks)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService);
 
-gateway.WithReference(gateway);
-
+gateway.WithReference(gateway).WithExternalHttpEndpoints();
 
 // Scheduled Job Host ...
 builder.AddProject<Projects.AdminAssistant_Hangfire>(Constants.Services.ScheduledJobHost)
     .WithReference(gateway)
     .WithReference(scheduledJobDatabase)
-    .WithHealthCheck()
     .WaitFor(databaseMigrationWorkerService); // Should be waiting for gateway in the long run
 
 // Main App ...
 builder.AddProject<Projects.AdminAssistant_ServerSideBlazor>(Constants.ServerAppName)
-    .WithReference(gateway)
-    .WithHealthCheck();
+    .WithReference(gateway);
 
 // Retro console UI ... :-)
 builder.AddProject<Projects.AdminAssistant_Retro>(Constants.Services.RetroConsole)
