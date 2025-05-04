1dff7eb2091b0a004a70423b96623950a5770e7d
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2025-05-04 19:01:13 +0100

    old code cleanup

--- src/AdminAssistant.WPF/Modules/DashboardModule/DashboardComponent.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace AdminAssistant.WPF.Modules.DashboardModule
-{
-    public partial class DashboardComponent : UserControl
-    {
-        public DashboardComponent() => InitializeComponent();
-    }
-}
--- src/AdminAssistant.WPF/Modules/DocumentsModule/DocumentsComponent.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows.Controls;
-
-namespace AdminAssistant.WPF.Modules.DocumentsModule
-{
-    public partial class DocumentsComponent : UserControl
-    {
-        public DocumentsComponent() => InitializeComponent();
-    }
-}
