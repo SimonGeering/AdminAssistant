deaa127bf626fd6055dc020f5be0f82337f52c99
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2025-11-15 22:03:43 +0000

    implement full dispose patern

--- src/AdminAssistant.Web/Shared/AdminAssistantComponentBase.cs
+++ src/AdminAssistant.Web/Shared/AdminAssistantComponentBase.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components;
 
 namespace AdminAssistant.Blazor.Client.Shared;
 
-public abstract class AdminAssistantComponentBase<TRuntimeViewModel, TDesignViewModel> : ComponentBase
+public abstract class AdminAssistantComponentBase<TRuntimeViewModel, TDesignViewModel> : ComponentBase, IDisposable
     where TRuntimeViewModel : class, IViewModelBase
     where TDesignViewModel : class, TRuntimeViewModel
 {
+    private bool _disposed;
+    private void vmOnPropertyChanged(object? sender, EventArgs e) => StateHasChanged();
+
     [Inject] protected NavigationManager Nav { get; set; } = null!;
     [Inject] protected IServiceProvider Services { get; set; } = null!;
 
@@ -14,7 +17,7 @@ public abstract class AdminAssistantComponentBase<TRuntimeViewModel, TDesignView
 
     protected bool IsDesignerDemo { get; private set; } = false;
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
         IsDesignerDemo = Nav.Uri.Contains("/demo/", StringComparison.OrdinalIgnoreCase);
 
@@ -22,9 +25,33 @@ public abstract class AdminAssistantComponentBase<TRuntimeViewModel, TDesignView
             ? Services.GetRequiredService<TDesignViewModel>()
             : Services.GetRequiredService<TRuntimeViewModel>();
 
-        vm.PropertyChanged += (_, _) => StateHasChanged();
-        base.OnInitialized();
+        vm.PropertyChanged += vmOnPropertyChanged;
+
+        await vm.OnInitializedAsync();
+        await base.OnInitializedAsync();
     }
-    protected override Task OnInitializedAsync() => vm.OnInitializedAsync();
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) return;
+
+        if (disposing && vm != null)
+        {
+            vm.PropertyChanged -= vmOnPropertyChanged;
+        }
+
+        _disposed = true;
+    }
+
+    ~AdminAssistantComponentBase()
+    {
+        Dispose(false);
+    }
 }
 
