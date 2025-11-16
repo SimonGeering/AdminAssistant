1ecb2f6615a638ea70a8504db81509c94647b047
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2025-11-16 14:30:17 +0000

    Rename AdminAssistantModuleComponentBase to AdminAssistantPageComponentBase

--- src/AdminAssistant.Web/Modules/AdminAssistantPageComponentBase.cs
+++ src/AdminAssistant.Web/Modules/AdminAssistantPageComponentBase.cs
@@ -2,7 +2,7 @@ using AdminAssistant.Blazor.Client.Shared;
 
 namespace AdminAssistant.Blazor.Client.Modules;
 
-public abstract class AdminAssistantModuleComponentBase<TRuntimeViewModel, TDesignerViewModel>
+public abstract class AdminAssistantPageComponentBase<TRuntimeViewModel, TDesignerViewModel>
     : AdminAssistantComponentBase<TRuntimeViewModel, TDesignerViewModel>
     where TRuntimeViewModel : class, IViewModelBase
     where TDesignerViewModel : class, TRuntimeViewModel
